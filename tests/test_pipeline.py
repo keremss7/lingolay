765b830bb4fa025e62640b5ce0b4ffd537937759
keremss7 <108955865+keremss7@users.noreply.github.com>
2026-09-24 16:00:20 +0300

    test: tear down Qt objects deterministically (fixes exit crash on Windows CI)

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -1,6 +1,6 @@
 def test_extract_new_sentences_skips_already_translated(qapp):
     from lingolay.core.pipeline import Pipeline
-    p = Pipeline()
+    p = Pipeline(qapp)
     assert p._extract_new_sentences('Hello there. How are you?') == 'Hello there. How are you?'
     # scrolling subtitle: first line already translated, only the new one is returned
     assert p._extract_new_sentences('How are you? I am fine.') == 'I am fine.'
@@ -14,7 +14,7 @@ def test_own_translation_is_detected(qapp):
     import time
 
     from lingolay.core.pipeline import Pipeline
-    p = Pipeline()
+    p = Pipeline(qapp)
     p._recent_translations.append((p._flatten('Beni takip edin, köprü çökmek üzere!'), time.time()))
     assert p._is_own_translation('Beni takip edin, kopru çökmek üzere!')
     assert not p._is_own_translation('Something completely different here')
@@ -26,3 +26,6 @@ def test_main_window_builds(qapp):
     assert w.windowTitle() == 'Lingolay'
     w._hotkeys.stop()
     w._overlay.close()
+    w._region_selector.close()
+    w.deleteLater()
+    qapp.processEvents()
